b10e814614a6bfae44e7f4336d15d8fc1d3779b8
Jayphen <software@jayphen.com>
2021-03-09 12:25:52 +0100

    Add vercel config

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,22 @@
-import sirv from 'sirv';
-import express from 'express';
-import compression from 'compression';
-import * as sapper from '@sapper/server';
-import 'dotenv/config'
+import sirv from "sirv";
+import express from "express";
+import compression from "compression";
+import * as sapper from "@sapper/server";
+import "dotenv/config";
 
-const {PORT, NODE_ENV} = process.env;
-const dev = NODE_ENV === 'development';
+const { PORT, NODE_ENV } = process.env;
+const dev = NODE_ENV === "development";
 
-const app = express()
-	.use(
-		express.json(),
-		express.urlencoded(),
-		compression({threshold: 0}),
-		sirv('static', {dev}),
-		sapper.middleware()
-	);
+const app = express().use(
+  express.json(),
+  express.urlencoded(),
+  compression({ threshold: 0 }),
+  sirv("static", { dev }),
+  sapper.middleware()
+);
 
 export default app;
 
 if (!process.env.NOW_REGION) {
-	app.listen(PORT);
+  app.listen(PORT);
 }
-
