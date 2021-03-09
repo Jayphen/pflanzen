a58b44633d6024289825bfc43cc2544c4f02be36
Jayphen <software@jayphen.com>
2021-03-09 15:09:49 +0100

    Add index

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,23 +1,36 @@
 <script lang="ts">
-	import Nav from "../components/Nav.svelte";
+  import Nav from "../components/Nav.svelte";
 
-	export let segment: string;
+  export let segment: string;
 </script>
 
-<Nav {segment} />
+<div class="layout">
+  <Nav {segment} />
 
-<main>
-	<slot />
-</main>
+  <main>
+    <div class="content">
+      <slot />
+    </div>
+  </main>
+</div>
 
 <style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: #afd2a3;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+  :global(html, body, #sapper) {
+    height: 100%;
+  }
+  .layout {
+    display: grid;
+    height: 100%;
+    grid-template-rows: auto 1fr;
+  }
+  main {
+    position: relative;
+    background-color: #afd2a3;
+    box-sizing: border-box;
+  }
+  .content {
+    max-width: 56em;
+    margin: 0 auto;
+    padding: 2em;
+  }
 </style>
-
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,9 @@
-eyy
+<p>Hello! If you are reading this, please do not water my plants.</p>
+
+<style>
+  p {
+    padding: 2em;
+    border-radius: 1em;
+    background: white;
+  }
+</style>
