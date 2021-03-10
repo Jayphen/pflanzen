c74ac2efeba9421bb907306fc0510680fceb9fa3
Jayphen <software@jayphen.com>
2021-03-10 18:55:39 +0100

    Set trust proxy

--- src/server.ts
+++ src/server.ts
@@ -9,21 +9,23 @@ import { setCacheHeaders } from "./middleware/setCacheHeaders";
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development";
 
-const app = express().use(
-  cookieSession({
-    name: "session",
-    secret: process.env.SECRET,
-    // Cookie Options
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 24 hours
-    sameSite: "strict",
-  }),
-  express.json(),
-  setCacheHeaders,
-  express.urlencoded({ extended: true }),
-  compression({ threshold: 0 }),
-  sirv("static", { dev }),
-  sapper.middleware({ session: (req) => ({ auth: !!req.session.auth }) })
-);
+const app = express()
+  .set("trust proxy", true)
+  .use(
+    cookieSession({
+      name: "session",
+      secret: process.env.SECRET,
+      // Cookie Options
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 24 hours
+      sameSite: "strict",
+    }),
+    express.json(),
+    setCacheHeaders,
+    express.urlencoded({ extended: true }),
+    compression({ threshold: 0 }),
+    sirv("static", { dev }),
+    sapper.middleware({ session: (req) => ({ auth: !!req.session.auth }) })
+  );
 
 export default app;
 
