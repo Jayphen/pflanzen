17daf1e8eb1f68f74ccabcb071d08fbf6bfb4a4a
Jayphen <software@jayphen.com>
2021-03-11 11:12:44 +0100

    Update Airtable types

--- src/airtable.d.ts
+++ src/airtable.d.ts
@@ -1,31 +1,33 @@
 import type Record from "airtable/lib/record";
 
 type Thumbnail = {
-	url: string;
-	width: number;
-	height: number;
-}
+  url: string;
+  width: number;
+  height: number;
+};
 interface Image {
-	filename: string;
-	id: string;
-	size: number;
-	thumbnails: {
-		full: Thumbnail,
-		large: Thumbnail,
-		small: Thumbnail
-	};
-	type: string;
-	url: string;
+  filename: string;
+  id: string;
+  size: number;
+  thumbnails: {
+    full: Thumbnail;
+    large: Thumbnail;
+    small: Thumbnail;
+  };
+  type: string;
+  url: string;
 }
 
 interface AirtableRecord<T> {
-	fields: T
-	createdTime: string;
-	id: string;
+  fields: T;
+  createdTime: string;
+  id: string;
 }
 
 type PlantField = {
-	Name: string;
-	"Last Watered": string;
-	'Images': Image[]
-}
+  Name?: string;
+  "Last Watered"?: string;
+  Images?: Image[];
+  Status?: "Alive" | "Sad" | "Dead";
+  Notes?: string;
+};
